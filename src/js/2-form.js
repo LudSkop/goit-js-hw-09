c9072e487abc779fb149d475dc79e3eedeeca477
LudSkop <luda.skopenko80@gmail.com>
2026-02-02 20:33:58 +0200

    added 2-form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,2 +1,12 @@
+formData = {
+    email: "",
+    message: "",
+}
 
-console.log('Form module loaded');
+const formFech = document.querySelector('.feedback-form');
+formFech.addEventListener('input', onFormInput);
+function onFormInput (event) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+    
+}
